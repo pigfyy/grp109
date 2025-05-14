fb95a3edeba4d89de5bdfc7d92fabdb2192e5a57
divyalakshana01 <divyalakshanar@gmail.com>
2025-05-14 00:58:14 -0700

    Update script.js

--- members/script.js
+++ members/script.js
@@ -1,1 +1,22 @@
 // add js here
+function displayInfo() {
+
+   var select = document.getElementById('member-dropdown');
+
+   // Hide All members content by default
+   for (let i = 0; i < select.options.length; i++) {
+      const memberId = select.options[i].value;
+      const memberDiv = document.getElementById(memberId);
+      memberDiv.style.display = "none";
+   }
+
+   //Get the member element for selected value and display it
+   var selectedValue = select.value;
+   var memberElement = document.getElementById(selectedValue);
+   memberElement.style.display = "block";
+
+   //change the css file based on selected value
+   var styleLink = document.getElementById('member-css');
+   styleLink.href = `./css/${selectedValue}.css`;
+   console.log(styleLink.href);
+}

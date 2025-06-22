9c9202aa7daedaf46a7edc713a05fde965267dcf
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-06-21 22:23:03 -0700

    trying to fix mixed content error

--- js/universal.js
+++ js/universal.js
@@ -3,7 +3,7 @@
     All pages are a group effort and have multiple contributors -->
 
 /* Creates a header we can use for every page */
-let header = document.createElement('header');
+let header = document.createElement("header");
 header.innerHTML = `
 <div>
     <h1>Artisanal Slice</h1>
@@ -22,26 +22,26 @@ header.innerHTML = `
 `;
 
 /* Creates a footer we can use for every page */
-let footer = document.createElement('footer');
+let footer = document.createElement("footer");
 footer.innerHTML = `
 <p>&copy; 2025 Artisanal Slice. All rights reserved.</p>
 <div style="text-align: center">
     <a href="https://jigsaw.w3.org/css-validator/" target="_blank" alt="W3 Validator">
-        <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"/>
+        <img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!"/>
     </a>
-    <a href="http://validator.w3.org/check?uri=referer">
-        <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict"/>
+    <a href="https://validator.w3.org/check?uri=referer">
+        <img src="https://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict"/>
     </a>
 </div>
 `;
 
 /*  Cycles through each link and adds a specific CSS class to
     the current one */
-header.querySelectorAll('a').forEach(link => {
-    if (link.pathname == window.location.pathname) {
-        link.parentElement.classList.add('active-url');
-    }
-})
+header.querySelectorAll("a").forEach((link) => {
+  if (link.pathname == window.location.pathname) {
+    link.parentElement.classList.add("active-url");
+  }
+});
 
 /*  Adds the header as the first element under the body tag.
     This is a major QOL fix for us, as we previously had a
